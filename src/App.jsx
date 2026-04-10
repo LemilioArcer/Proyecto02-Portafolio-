58d01d196a3651a57ff4dba0312bc39f6fca894d
Arce <emilioarce1285@gmail.com>
2026-04-10 00:41:27 -0600

    Nimiedad encontrada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Hero from "./components/hero";
-import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
