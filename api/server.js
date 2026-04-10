a970c16b1abd7ff71d4735c5edcc5ca212a1c9ab
Arce <emilioarce1285@gmail.com>
2026-04-09 21:54:52 -0600

    Second version

---
+++ api/server.js
@@ -0,0 +1,31 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const handler = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Wrong method' });
+  }
+
+  try {
+    const { name, message, email } = req.body;
+
+    const data = await resend.emails.send({
+      from: 'Portafolio interest <onboarding@resend.dev>',
+      to: 'emilioarce1285@gmail.com',
+      subject: `New message from ${name}`,
+      html: `
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong></p>
+        <p>${message}</p>
+      `,
+    });
+
+    return res.status(200).json({ success: true, data });
+  } catch (error) {
+    return res.status(500).json({ success: false, error });
+  }
+};
+
+export default handler;
